d8287e25d40f7665c2d8c37a073bef5fa3af3fb0
ericj376 <ericellingson376@gmail.com>
2016-02-29 14:10:27 -0700

    added email_password

--- server.js
+++ server.js
@@ -9,10 +9,38 @@ var blogRouter = require('./routes/blogs');
 
 var Blog = require('./models/blogs')
 
+var session = require('express-session');
+
+var passport = require('passport');
+
+var flash = require('connect-flash');
+
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
+app.use(session({
+ secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(session({
+ cookie: {
+   maxAge: 60000
+ }
+}));
+app.use(flash());
+
+require('./config/passport')(passport);
+// routes ======================================================================
+require('./routes/user.js')(app, passport);
+
 app.use(express.static('public'));
+
+app.use(function(req, res, next){
+	var user = req.user || "no user";
+	console.log(user);
+	next();
+});
 
 app.set('view engine', 'ejs');
 
